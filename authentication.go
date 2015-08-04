d9a7ee3f96f07731d48a8d1b610ec0e57f5c0b7b
neilcwilkinson <neilcwilkinson@hotmail.com>
2015-08-04 11:05:40 -0400

    Initial Load

---
+++ authentication.go
@@ -0,0 +1,33 @@
+package authentication
+
+import(
+	"strings" 
+	"encoding/json"
+	"fmt"
+	//custom
+	"common/messaging"
+)
+
+type User struct {
+  Username string
+  Password string
+}
+
+var Lu = make(chan int)
+
+func IsAuthorized(message messaging.Message) (bool, User) {
+	var user User
+
+	if strings.EqualFold(message.Subject, "Authorization") {	
+    	byt2 := []byte(message.Body)
+    	json.Unmarshal(byt2, &user)
+		fmt.Println("User: ", user.Username)
+        
+        if strings.EqualFold(user.Username, "neil") {
+        	return true, user
+        } else {
+        	return false, user
+        }
+    }
+    return false, user
+}
